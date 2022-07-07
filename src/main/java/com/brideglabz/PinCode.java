74952870eb69156bcc05feb8da72ae946bea20df
ChetanGavanali <chetangavanali@gmail.com>
2022-07-08 00:10:59 +0530

    Added UC1

--- src/main/java/com/brideglabz/PinCode.java
+++ src/main/java/com/brideglabz/PinCode.java
@@ -1,8 +1,32 @@
 package com.brideglabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PinCode {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome To Pin Code Validator");
+    public static boolean checkValidPinCode(String pinCode){
+
+        String pinCodeRegex = "[0-9]{6}$";
+        Pattern patternObject = Pattern.compile(pinCodeRegex);
+        if (pinCode == null) {
+            return false;
+        }
+        Matcher matcherObject = patternObject.matcher(pinCode);
+        return matcherObject.matches();
     }
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To Pin Code Validator");
+
+        String pinCode = "400088";
+        boolean isPinCode = checkValidPinCode(pinCode);
+
+        if(isPinCode)
+            System.out.println(pinCode+" is a Valid PinCode");
+        else
+            System.out.println(pinCode+" is an Invalid PinCode");
+    }
+
 }
