8b412d1f649516ba9ff2881a4fc3d8c9ffc25f38
ChetanGavanali <chetangavanali@gmail.com>
2022-07-08 00:16:02 +0530

    Added UC2

--- src/main/java/com/brideglabz/PinCode.java
+++ src/main/java/com/brideglabz/PinCode.java
@@ -5,28 +5,33 @@ import java.util.regex.Pattern;
 
 public class PinCode {
 
-    public static boolean checkValidPinCode(String pinCode){
+    public static void checkValidPinCode(String pinCode){
 
-        String pinCodeRegex = "[0-9]{6}$";
+        boolean isPinCode;
+        String pinCodeRegex = "^[0-9]{6}$";
         Pattern patternObject = Pattern.compile(pinCodeRegex);
         if (pinCode == null) {
-            return false;
+            isPinCode = false;
         }
         Matcher matcherObject = patternObject.matcher(pinCode);
-        return matcherObject.matches();
-    }
-
-    public static void main(String[] args) {
-
-        System.out.println("Welcome To Pin Code Validator");
-
-        String pinCode = "400088";
-        boolean isPinCode = checkValidPinCode(pinCode);
+        isPinCode =  matcherObject.matches();
 
         if(isPinCode)
             System.out.println(pinCode+" is a Valid PinCode");
         else
             System.out.println(pinCode+" is an Invalid PinCode");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(" Welcome To Pin Code Validator");
+
+        String pinCode = "400088";
+        checkValidPinCode(pinCode);
+        pinCode = "A400088";
+        checkValidPinCode(pinCode);
+
+
     }
 
 }
